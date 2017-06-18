910ec13161f33e8792ec57775745c6d8ab84a988
Jonathan M. Lofton <jonathanlofton91@gmail.com>
2017-06-17 17:58:55 -0700

    Functional book create, delete webpage

--- W4D1/Homework/library/app/controllers/books_controller.rb
+++ W4D1/Homework/library/app/controllers/books_controller.rb
@@ -1,18 +1,50 @@
 class BooksController < ApplicationController
   def index
-    # your code here
+    # collects all the books in the
+    # book class currently
+    @books = Book.all
+
+
+    render :index
   end
 
   def new
-    # your code here
+    # just render the page with the form to add a book
+    render :new
   end
 
   def create
-    # your code here
+    # will actually save that new book with parameters
+    # when we give it to the database
+
+    # This will create a new book object with
+    # the params that were entered
+
+    book = Book.new(book_params)
+    if book.save
+      # will redirect you to the same
+      # page you were on so you can see
+      # the added page
+      redirect_to books_url
+    else
+
+      #
+      flash.now[:errors] = book.errors.full_messages
+      render :new
+    end
   end
 
   def destroy
-    # your code here
+    # Going into the book class and finding
+    # and deleting that book
+    book = Book.find(params[:id])
+    book.destroy
+
+
+    # after deleting a book will
+    # redirect you back to the book page
+    # with that book now deleted
+    redirect_to books_url
   end
 
   private
