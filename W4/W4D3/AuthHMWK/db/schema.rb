f32d150497fbb125909ee679df745bf9bbbd0a0a
Jonathan M. Lofton <jonathanlofton91@gmail.com>
2017-06-21 13:06:39 -0700

    W4D3 Auth HMWK update

--- W4/W4D3/AuthHMWK/db/schema.rb
+++ W4/W4D3/AuthHMWK/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170621164951) do
+ActiveRecord::Schema.define(version: 20170621200028) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20170621164951) do
     t.string   "session_token",   null: false
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "password"
     t.index ["session_token"], name: "index_users_on_session_token", unique: true, using: :btree
     t.index ["username"], name: "index_users_on_username", unique: true, using: :btree
   end
