61d8417702a38a91253bb091cac3d2cc791381c5
Jonathan M. Lofton <jonathanlofton91@gmail.com>
2017-06-21 12:54:16 -0700

    W4D3 HMWK Authentication, implemented User auth methods

--- W4/W4D3/AuthHMWK/app/models/user.rb
+++ W4/W4D3/AuthHMWK/app/models/user.rb
@@ -5,19 +5,38 @@ class User < ApplicationRecord
   validates :password, ;ength: { minimum: 6, allow_nil: true }
   before_validation :ensure_session_token
 
-  def self.find_by_credentials
-
+  def self.find_by_credentials(username, password)
+    # find the user by User.find_by(username: username)
+    user = User.find_by(username: username)
+    # return user if user is found AND that user has a password_digest that is found
+    return user if user && BCrypt::Password.new(user.password_digest).is_password?(password)
+    nil
   end
 
   def self.generate_session_token
+    # returns a 16 character length password string
+    SecureRandom::urlsafe_base64(16)
   end
 
   def reset_session_token!
+    # generates a new session_token and saves it to the
+    # current users session_token variable
+    self.session_token = User.generate_session_token
+    # saves the Users current variables
+    self.save!
+    # returns the newly created session_token
+    self.session_token
   end
 
   def ensure_session_token
+    # if users token is nil give it a new session token
+    self.session_token ||= User.generate_session_token
   end
 
-  def password
-  end 
+  def password(password)
+    # sets the password given
+    @password = password
+    # creates a password_digest from the password given
+    self.password_digest = BCrypt::Password.create(password)
+  end
 end
