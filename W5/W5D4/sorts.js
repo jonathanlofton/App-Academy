a3636b9d46948efedd73c18eca5e661f208a44d3
Jonathan M. Lofton <jonathanlofton91@gmail.com>
2017-06-29 22:35:03 -0700

    Updated

--- W5/W5D4/sorts.js
+++ W5/W5D4/sorts.js
@@ -6,18 +6,21 @@ function bsearch(array, target) {
 
   let mid = Math.floor(array.length / 2);
 
+  let left = array.slice(0, mid);
+  let right = array.slice(mid + 1);
+
 
   if (array[mid] === target) {
     return mid;
   }
   else if (array[mid] > target ) {
-    let temp = bsearch(array.slice(mid + 1), target);
+    let temp = bsearch(left, target);
     return temp === -1 ? -1 : temp + 1 + mid
   }
   else if (array[mid] < target) {
-    return bsearch(array.slice(0, mid - 1), target);
+    return bsearch(right, target);
   }
 };
 
 
-console.log(bsearch([1,2,3,4,5,5,7,8,9], 3));
+console.log(bsearch([1,2,3,4,5,5,7,8,9], 1));
---
+++ W5/W5D5/webpack-demo/src/index.js
@@ -0,0 +1,12 @@
+import_from 'lodash';
+
+function component() {
+  var element = document.createElement('div');
+
+  // Lodash, now imported by this script
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  return element;
+}
+
+document.body.appendChild(component());
