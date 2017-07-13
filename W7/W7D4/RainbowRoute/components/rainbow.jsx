fa5f9cbb73cb1accf307522980d1f4a384400ebf
Jonathan M. Lofton <jonathanlofton91@gmail.com>
2017-07-12 19:54:07 -0700

    W7D4 homework up to Phase 2 completed

--- W7/W7D4/RainbowRoute/components/rainbow.jsx
+++ W7/W7D4/RainbowRoute/components/rainbow.jsx
@@ -16,8 +16,15 @@ class Rainbow extends React.Component {
       <div>
         <h1>Rainbow Router!</h1>
         {/* Your links should go here */}
-
+          <Link to="/red">Red</Link>
+          <Link to="/green">Green</Link>
+          <Link to="/blue">Blue</Link>
+          <Link to="/violet">Violet</Link>
         <div id="rainbow">
+          <Route path="/red" component={Red} />
+          <Route path="/green" component={Green} />
+          <Route path="/blue" component={Blue} />
+          <Route path="/violet" component={Violet} />
           {/* Your routes should go here */}
         </div>
       </div>
