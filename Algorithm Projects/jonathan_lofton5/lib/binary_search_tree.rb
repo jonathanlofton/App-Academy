ddcc0756011f67a36af87cd6c9533f8f9e1d6344
Jonathan M. Lofton <jonathanlofton91@gmail.com>
2017-08-21 17:38:28 -0700

    Algorithms 6 project graphs

--- Algorithm Projects/jonathan_lofton5/lib/binary_search_tree.rb
+++ Algorithm Projects/jonathan_lofton5/lib/binary_search_tree.rb
@@ -1,17 +1,29 @@
-# There are many ways to implement these methods, feel free to add arguments 
+# There are many ways to implement these methods, feel free to add arguments
 # to methods as you see fit, or to create helper methods.
+require_relative 'bst_node'
 
 class BinarySearchTree
+  attr_accessor :root
+
   def initialize
+    @root = nil
   end
 
   def insert(value)
+    if !@root
+      @root = BSTNode.new(value)
+      return
+    end
+
+    BinarySearchTree.insert!(@root, value)
   end
 
   def find(value, tree_node = @root)
+    BinarySearchTree.find!(tree_node, value)
   end
 
   def delete(value)
+    BinarySearchTree.find!(value)
   end
 
   # helper method for #delete:
@@ -19,7 +31,7 @@ class BinarySearchTree
   end
 
   def depth(tree_node = @root)
-  end 
+  end
 
   def is_balanced?(tree_node = @root)
   end
@@ -30,5 +42,41 @@ class BinarySearchTree
 
   private
   # optional helper methods go here:
+  def self.insert!(node, value)
+    # If there is no node, return a fresh one
+    if !node
+      return BSTNode.new(value)
+    end
 
+    # if there is a node then check the value of the node
+    # If the value of the node is larger than the new
+    # node to be inserted then put the new node to the left
+    # keep doing this until there isn't a node and place it there
+    if value >= node.value
+      node.right = BinarySearchTree.insert!(node.right, value)
+    else
+      node.left = BinarySearchTree.insert!(node.left, value)
+    end
+    node
+  end
+
+  def self.find!(node, value)
+    return nil unless node
+    return node if node.value == value
+
+    if value <= node.value
+      BinarySearchTree.find!(node.left, value)
+    else
+      BinarySearchTree.find!(node.right, value)
+    end
+  end
+
+  def self.max(node)
+  end
+
+  def self.delete_max!(node)
+  end
+
+  def self.delete!(node, value)
+  end
 end
