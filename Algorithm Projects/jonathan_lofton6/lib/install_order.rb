45ad1f14edb32df3ab180850cde9a54e6680d789
Jonathan M. Lofton <jonathanlofton91@gmail.com>
2017-08-22 11:42:18 -0700

    updated algo project 6

--- Algorithm Projects/jonathan_lofton6/lib/install_order.rb
+++ Algorithm Projects/jonathan_lofton6/lib/install_order.rb
@@ -1,4 +1,4 @@
-# Given an Array of tuples, where tuple[0] represents a package id,
+# Given an Array of tuples, where tuple[0] represents a package vertex,
 # and tuple[1] represents its dependency, determine the order in which
 # the packages should be installed. Only packages that have dependencies
 # will be listed, but all packages from 1..max_id exist.
@@ -11,8 +11,28 @@ require_relative 'graph'
 require_relative 'topological_sort'
 
 def install_order(arr)
-  hash = Hash.new()
-  arr.each do |tuple|
-    hash[tuple[1]] = tuple[0]
-  end 
+  tuple_nums = (1..arr.flatten.max).to_a
+  hash = Hash.new {[]}
+
+  arr.each { |tuple| hash[tuple[0]] += [tuple[1]] }
+
+  sorted = []
+
+  until queue.empty?
+    vertex = queue.shift
+    sorted << vertex
+
+    hash.keys.each do |vertex_depend|
+      hash[vertex_depend] -= [vertex]
+
+      if hash[vertex_depend].empty?
+        queue << vertex_depend
+        hash.delete(vertex_depend)
+      end
+
+    end
+
+  end
+
+  sorted
 end
