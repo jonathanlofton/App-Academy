7fe261788a8225f4aaa60020f0e32c72d419a319
Jonathan M. Lofton <jonathanlofton91@gmail.com>
2017-09-04 16:09:56 -0700

    refactored

--- princeton-algos/binary-search.rb
+++ princeton-algos/binary-search.rb
@@ -1,8 +1,22 @@
 class Node
 
   attr_accessor :left, :right
+  attr_reader :value
 
-  def initialize(data)
-    @data = data
+  def initialize(value)
+    @value = value
+    @left = nil
+    @right = nil
   end
+
+end
+
+class BinarySearchTree
+
+  attr_reader :root
+
+  def initialize(root)
+    @root = root
+  end
+
 end
