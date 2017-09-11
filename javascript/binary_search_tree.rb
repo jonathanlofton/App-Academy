2495851b7c425d4461905c2735c3266852bfbac8
Jonathan M. Lofton <jonathanlofton91@gmail.com>
2017-09-10 18:54:50 -0700

    bst find second minimum value

---
+++ javascript/binary_search_tree.rb
@@ -0,0 +1,9 @@
+def find_second_minimum_value(root, array = [])
+    return [] if root == nil
+
+    find_second_minimum_value(root.left, array)
+    array.push(root.val)
+    find_second_minimum_value(root.right, array)
+
+    array[-2]
+end
