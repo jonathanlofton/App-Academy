b3d460f82ee15c7308f73a812ba0f84955b00400
Jonathan M. Lofton <jonathanlofton91@gmail.com>
2017-09-17 16:19:54 -0700

    hash map finished

--- hashmap/lib/p04_linked_list.rb
+++ hashmap/lib/p04_linked_list.rb
@@ -19,7 +19,14 @@ class Node
 end
 
 class LinkedList
+
+  include Enumerable
+
   def initialize
+    @head = Node.new
+    @tail = Node.new 
+    @head.next = @tail 
+    @tail.prev = @head 
   end
 
   def [](i)
@@ -28,34 +35,82 @@ class LinkedList
   end
 
   def first
+    @head.next
   end
 
   def last
+    @tail.prev
   end
 
   def empty?
+    first == @tail
   end
 
   def get(key)
+    current_node = @head 
+    while current_node 
+      if current_node.key == key 
+        return current_node.val
+      end
+      current_node = current_node.next 
+    end 
   end
 
   def include?(key)
+    current_node = @head 
+    while current_node 
+      if current_node.key == key 
+        return true 
+      end 
+      current_node = current_node.next
+    end 
+    false
   end
 
   def append(key, val)
+    new_node = Node.new(key, val)
+    tail_prev = @tail.prev 
+    tail_prev.next = new_node
+    @tail.prev = new_node 
+    new_node.prev = tail_prev 
+    new_node.next = @tail
   end
 
   def update(key, val)
+    current_node = @head 
+    while current_node
+      if current_node.key == key 
+        current_node.val = val 
+      end 
+      current_node = current_node.next
+    end 
   end
 
   def remove(key)
+    current_node = @head 
+    prev_node = nil 
+    while current_node 
+      next_node = current_node.next
+      if current_node.key == key 
+        prev_node.next = current_node.next 
+        next_node.prev = prev_node
+      end
+      prev_node = current_node 
+      current_node = current_node.next 
+    end 
   end
 
   def each
+    current_node = @head.next 
+    array = []
+    while current_node != @tail
+      yield current_node
+      current_node = current_node.next
+    end
   end
 
   # uncomment when you have `each` working and `Enumerable` included
-  # def to_s
-  #   inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
-  # end
+  def to_s
+    inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
+  end
 end
--- hashmap/lib/p05_hash_map.rb
+++ hashmap/lib/p05_hash_map.rb
@@ -4,33 +4,51 @@ require_relative 'p04_linked_list'
 class HashMap
   attr_reader :count
 
+  include Enumerable
+
   def initialize(num_buckets = 8)
     @store = Array.new(num_buckets) { LinkedList.new }
     @count = 0
   end
 
   def include?(key)
+    @store[key.hash % num_buckets].include?(key)
   end
 
   def set(key, val)
+    if include?(key)
+      @store[key.hash % num_buckets].update(key, val)
+    else
+      @count += 1
+      resize! if @count > @store.length
+      @store[key.hash % num_buckets].append(key, val)
+    end 
   end
 
   def get(key)
+    @store[key.hash % num_buckets].get(key)
   end
 
   def delete(key)
+    @store[key.hash % num_buckets].remove(key)
+    @count -= 1
   end
 
   def each
+    @store.each do |linked_list| 
+      linked_list.each do |node|
+        yield [node.key, node.val]
+      end 
+    end 
   end
 
   # uncomment when you have Enumerable included
-  # def to_s
-  #   pairs = inject([]) do |strs, (k, v)|
-  #     strs << "#{k.to_s} => #{v.to_s}"
-  #   end
-  #   "{\n" + pairs.join(",\n") + "\n}"
-  # end
+  def to_s
+    pairs = inject([]) do |strs, (k, v)|
+      strs << "#{k.to_s} => #{v.to_s}"
+    end
+    "{\n" + pairs.join(",\n") + "\n}"
+  end
 
   alias_method :[], :get
   alias_method :[]=, :set
@@ -42,6 +60,14 @@ class HashMap
   end
 
   def resize!
+    num_buckets = @store.length * 2 
+    new_store = Array.new(num_buckets) { LinkedList.new }
+    @store.each do |linked_list| 
+      linked_list.each do |node|
+        new_store[node.key.hash % num_buckets].append(node.key, node.val)
+      end 
+    end 
+    @store = new_store
   end
 
   def bucket(key)
