90efb428f137c2bc575924980bd08fb7117f02b2
Jonathan M. Lofton <jonathanlofton91@gmail.com>
2017-09-17 13:43:13 -0700

    hashing function completed

--- hashmap/lib/p02_hashing.rb
+++ hashmap/lib/p02_hashing.rb
@@ -4,11 +4,21 @@ end
 
 class Array
   def hash
+    sum = 0
+    self.each_with_index do |num, idx|
+      sum += num.hash ^ idx
+    end 
+    sum
   end
 end
 
 class String
   def hash
+    sum = 0
+    self.chars.each_with_index do |ch, idx| 
+      sum += ch.ord.hash ^ idx 
+    end 
+    sum
   end
 end
 
@@ -16,6 +26,10 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    sum = 0 
+    self.each do |key, value|
+      sum += key.hash + value.hash
+    end 
+    sum 
   end
 end
