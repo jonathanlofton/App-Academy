1f250f6d2a3180f1df87fe3920367f18df024a7c
Jonathan M. Lofton <jonathanlofton91@gmail.com>
2017-10-03 15:33:52 -0700

    heap child and parent index algos completed

--- Algorithm Projects/Day_3_Heapsort/lib/heap.rb
+++ Algorithm Projects/Day_3_Heapsort/lib/heap.rb
@@ -21,9 +21,17 @@ class BinaryMinHeap
 
   public
   def self.child_indices(len, parent_index)
+    result = []
+    first_child = parent_index * 2 + 1
+    sec_child = parent_index * 2 + 2
+    result << first_child if first_child < len 
+    result << sec_child if sec_child < len 
+    result
   end
 
   def self.parent_index(child_index)
+    raise 'root has no parent' if child_index == 0
+    (child_index - 1) / 2
   end
 
   def self.heapify_down(array, parent_idx, len = array.length, &prc)
